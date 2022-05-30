8b92bca92f66e3a7709ac699a67fe36c501ee190
Matheus Fortini <matheusfortini7@gmail.com>
2022-05-30 14:55:00 -0300

    Esse commit cria as validações nos models de property e user

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,6 @@ class Property < ApplicationRecord
   belongs_to :user
 
   # Incluir as validações dos campos
+  validates :title, :description, :street_name, :city, :state, :country, presence: true
+  validates :price, presence: true, numericality: { only_float: true }
 end
