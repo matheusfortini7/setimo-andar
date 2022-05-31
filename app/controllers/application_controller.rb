18e6c1d7cdc16d81cf1405166926a8db1e4e9ae6
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-05-31 13:42:53 -0300

    correção application controller com pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
-  
+
+  after_action :verify_authorized, except: [:index, :show], unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: [:index, :show], unless: :skip_pundit?
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
 end
