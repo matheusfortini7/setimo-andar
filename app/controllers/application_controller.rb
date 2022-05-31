8820cd22d4e93cfa8af155f98b3b1bc6e409ff58
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-05-31 13:10:57 -0300

    inserida gema e inserido setup do application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!
+  include Pundit
+
+  after_action :verify_authorized, except: [:index, :show], unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: [:index, :show], unless: :skip_pundit?
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
 end
