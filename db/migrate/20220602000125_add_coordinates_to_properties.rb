d3d72360290f02f15ab86ef2bb4fe92aa01db27d
igor ricci <assis.ricci@gmail.com>
2022-06-01 21:14:05 -0300

    add info to popup

--- db/migrate/20220602000125_add_coordinates_to_properties.rb
+++
@@ -1,6 +0,0 @@
-class AddCoordinatesToProperties < ActiveRecord::Migration[6.1]
-  def change
-    add_column :properties, :latitude, :float
-    add_column :properties, :longitude, :float
-  end
-end
