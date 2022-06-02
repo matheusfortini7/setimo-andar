8c4fba2ad49ccf98c8012a5e0e0022592fd7065d
igor ricci <assis.ricci@gmail.com>
2022-06-01 21:09:52 -0300

    add controller methods to show and import style to application scss

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -14,5 +14,37 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
+
+    this.#addMarkersToMap();
+    this.#fitMapToMarkers();
+  }
+
+  // #addMarkersToMap() {
+  //   this.markersValue.forEach((marker) => {
+  //     const popup = new mapboxgl.Popup().setHTML(marker.info_window)
+
+  //     new mapboxgl.Marker()
+  //       .setLngLat([ marker.lng, marker.lat ])
+  //       .setPopup(popup)
+  //       .addTo(this.map)
+  //   });
+  // }
+
+  #addMarkersToMap() {
+    new mapboxgl.Marker()
+      .setLngLat([ this.markersValue.lng, this.markersValue.lat ])
+      .addTo(this.map)
+  }
+
+  // #fitMapToMarkers() {
+  //   const bounds = new mapboxgl.LngLatBounds()
+  //   this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+  //   this.map.fitBounds(bounds, { padding: 70, maxZoom: 10, duration: 1500 })
+  // }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    bounds.extend([ this.markersValue.lng, this.markersValue.lat ])
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 10, duration: 1500 })
   }
 }
